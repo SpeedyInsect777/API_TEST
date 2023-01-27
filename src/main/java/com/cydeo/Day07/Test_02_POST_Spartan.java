d5903a384157d38567ac2cae9a38c25b6c4102c5
SpeedyInsect777 <manasoev@gmail.com>
2023-01-26 23:42:05 -0500

    API updates

--- src/main/java/com/cydeo/Day07/Test_02_POST_Spartan.java
+++ src/main/java/com/cydeo/Day07/Test_02_POST_Spartan.java
@@ -34,7 +34,7 @@ public class Test_02_POST_Spartan extends SpartanTestBase {
 
     @DisplayName("POST Spartan with String body")
     @Test
-    public void test01(){
+    public void test01() {
 
         String newSpartan = " {\n" +
                 "     \"gender\":\"Male\",\n" +
@@ -49,38 +49,92 @@ public class Test_02_POST_Spartan extends SpartanTestBase {
                 contentType("application/json").body(newSpartan).when().post("api/spartans/").
                 then().statusCode(201).extract().jsonPath();
 
-        assertEquals("John Doe",jsonPath.getString("data.name"));
-       assertEquals("Male",jsonPath.getString("data.gender"));
-       assertEquals(8877445596l,jsonPath.getLong("data.phone"));
-        System.out.println("id: "+jsonPath.getInt("data.id"));
+        assertEquals("John Doe", jsonPath.getString("data.name"));
+        assertEquals("Male", jsonPath.getString("data.gender"));
+        assertEquals(8877445596l, jsonPath.getLong("data.phone"));
+        System.out.println("id: " + jsonPath.getInt("data.id"));
 
     }
+
     @DisplayName("POST Spartan with POJO")
     @Test
-    public void test02(){
+    public void test02() {
 
         Spartan newSpartan = new Spartan();
         newSpartan.setName("John Pojo");
         newSpartan.setGender("Male");
         newSpartan.setPhone(8877445596l);
 
-
-
-        String message = "A Spartan is Born";
-
-
-
         JsonPath jsonPath = given().accept(ContentType.JSON).
                 and().log().body().
                 contentType("application/json").
                 body(newSpartan).//   ! ! !  Serialization  ! ! !
-                when().post("api/spartans/").
+                        when().post("api/spartans/").
                 then().statusCode(201).extract().jsonPath();
 
-        assertEquals(newSpartan.getName(),jsonPath.getString("data.name"));
-        assertEquals(newSpartan.getGender(),jsonPath.getString("data.gender"));
-        assertEquals(newSpartan.getPhone(),jsonPath.getLong("data.phone"));
+        assertEquals(newSpartan.getName(), jsonPath.getString("data.name"));
+        assertEquals(newSpartan.getGender(), jsonPath.getString("data.gender"));
+        assertEquals(newSpartan.getPhone(), jsonPath.getLong("data.phone"));
 
+    }
 
+    @DisplayName("POST Spartan with POJO and GET {id}")
+    @Test
+    public void test03() {
+
+        Spartan newSpartan = new Spartan();
+        newSpartan.setName("John Pojo");
+        newSpartan.setGender("Male");
+        newSpartan.setPhone(8877445596l);
+        String message = "A Spartan is Born";
+        int newSpartanId = given().accept(ContentType.JSON).
+                and().log().body().
+                contentType("application/json").
+                body(newSpartan).//   ! ! !  Serialization  ! ! !
+                        when().post("api/spartans/").
+                then().statusCode(201).extract().jsonPath().getInt("data.id");
+
+    }
+
+    @DisplayName("POST Spartan with POJO and POJO")
+    @Test
+    public void test04() {
+
+        Spartan newSpartan = new Spartan();
+        newSpartan.setName("John Pojo");
+        newSpartan.setGender("Male");
+        newSpartan.setPhone(8877445596l);
+
+        int newSpartanId = given().accept(ContentType.JSON).
+                and().log().body().
+                contentType("application/json").
+                body(newSpartan).//   ! ! !  Serialization  ! ! !
+                        when().post("api/spartans/").
+                then().statusCode(201).extract().jsonPath().getInt("data.id");
+
+    }
+
+    @DisplayName("GET new Spartan with POJO")
+    @Test
+    public void test05() {
+
+        Spartan newSpartan = new Spartan();
+        newSpartan.setName("Jackson");
+        newSpartan.setGender("Male");
+        newSpartan.setPhone(1234567890l);
+
+        int newSpartanId = given().accept(ContentType.JSON).
+                and().log().body().
+                contentType("application/json").
+                body(newSpartan).//   ! ! !  Serialization  ! ! !
+                        when().post("api/spartans/").
+                then().statusCode(201).extract().jsonPath().getInt("data.id");
+
+        Spartan mySpartan = given().accept(ContentType.JSON).
+                contentType("application/json").
+                pathParam("id", newSpartanId).
+                when().get("api/spartans/{id}").
+                then().statusCode(200).extract().jsonPath().getObject("", Spartan.class);
+        System.out.println(mySpartan);
     }
 }
